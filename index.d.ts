6885feef70bab21cea517bed23ccd0e81796c189
Meriegg <ya.boybeats1234@gmail.com>
2023-06-27 17:28:33 +0300

    Added a typescript declaration file

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+declare module "@mariodev14/socials" {
+  export const CONTACT_EMAIL: string;
+  export const WEBSITE_LINK: string;
+  export const WEBSITE_CONTACT_LINK: string;
+  export const INSTAGRAM_USERNAME: string;
+  export const GITHUB_USERNAME: string;
+  export const TWITTER_USERNAME: string;
+  export const INSTAGRAM_LINK: string;
+  export const GITHUB_LINK: string;
+  export const TWITTER_LINK: string;
+  export const EMAIL_LINK: string;
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const CONTACT_EMAIL = "mario.developer.contact@gmail.com";
 
+const WEBSITE_LINK = "https://mariodev.vercel.app";
+const WEBSITE_CONTACT_LINK = `${WEBSITE_LINK}/contact`;
 const INSTAGRAM_USERNAME = "meriofrog";
 const GITHUB_USERNAME = "Meriegg";
 const TWITTER_USERNAME = "Mario_Dev14";
@@ -10,6 +12,8 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_USERNAME}`;
 const EMAIL_LINK = `mailto:${CONTACT_EMAIL}`;
 
 module.exports = {
+  WEBSITE_LINK,
+  WEBSITE_CONTACT_LINK,
   CONTACT_EMAIL,
   INSTAGRAM_USERNAME,
   GITHUB_USERNAME,
@@ -17,5 +21,6 @@ module.exports = {
   INSTAGRAM_LINK,
   GITHUB_LINK,
   TWITTER_LINK,
-  EMAIL_LINK
-}
+  EMAIL_LINK,
+};
+
